9fa1037a67cf8e31d84c8150865a19fe50405731
Alan Tan <dodieboy@users.noreply.github.com>
2026-04-06 20:29:04 +0800

    Fix typo

--- backend/services/composite/book-drone/app/run.py
+++ backend/services/composite/book-drone/app/run.py
@@ -334,5 +334,5 @@ def validate_route():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
+if __name__ == "__main":
     app.run(host="0.0.0.0", port=8101, debug=True)
